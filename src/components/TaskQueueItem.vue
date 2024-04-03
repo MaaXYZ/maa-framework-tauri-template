974435eecc716e7e58506f1ea69aba555c8b3961
KevinT3Hu <kevint3hu@outlook.com>
2024-04-03 11:18:16 +0800

    feat: add status indicator for task state

--- src/components/TaskQueueItem.vue
+++ src/components/TaskQueueItem.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import CloseIcon from "@/assets/icons/CloseIcon.vue";
+import DoneIcon from "@/assets/icons/DoneIcon.vue";
 import TaskStatus from "@/interface/TaskStatus";
 import { useTaskQueueStore } from "@/stores/TaskQueueStore";
 
@@ -17,13 +19,20 @@ function removeCurrent() {
 
 <template>
     <div ref="outer" class="item mx-1 text-center items-center shadow relative">
-        <mdui-dropdown trigger="contextmenu" placement="bottom-end">
+        <mdui-dropdown :trigger="task.state==='Running' ? 'manual' : 'contextmenu' " placement="bottom-end">
             <mdui-card
-                clickable
+                :clickable="task.state === 'Pending'"
                 slot="trigger"
                 class="flex flex-col w-full h-full layer"
             >
-                <p class="text-center">{{ props.task.taskType }}</p>
+                <p class="text-center">
+                    <mdui-icon v-if="task.state==='Completed'">
+                        <DoneIcon />
+                    </mdui-icon>
+                    <mdui-icon v-if="task.state==='Failed'" style="color: red;">
+                        <CloseIcon />
+                    </mdui-icon>
+                    {{ props.task.taskType }}</p>
                 <mdui-linear-progress
                     v-if="props.task.state === 'Running'"
                 ></mdui-linear-progress>
